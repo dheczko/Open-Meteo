67b6d8e38394dfd0f1b968504cbb0b02a4321597
dheczko <daniel.heczko20@gmail.com>
2026-06-20 21:45:24 +0200

    Add week days section

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 import tkinter as tk
 
 
+# Functions
+
+def toggle_all_days():
+    global all_selected
+
+    all_selected = not all_selected
+
+    for var in days_vars.values():
+        var.set(all_selected)
+    
+    toggle_all_button.config(
+        text="ALL" if not all_selected else "NONE"
+    )
+
+
 # Interface sections
 
 def create_time_section(root):
@@ -62,6 +77,61 @@ def create_time_section(root):
     date_to.pack(side="left")
 
 
+def create_week_days_section(root):
+    global days_vars, all_selected, toggle_all_button
+
+    days_frame = tk.LabelFrame(root, text="Week days")
+    days_frame.pack(padx=15, pady=5, fill="x")
+
+    days_vars = {}
+
+    week_days = [
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday"
+    ]
+
+    for col in range(2):
+        days_frame.grid_columnconfigure(col, weight=1)
+
+    for i, name in enumerate(week_days):
+        var = tk.BooleanVar(value=True)
+        days_vars[name] = var
+
+        tk.Checkbutton(
+            days_frame,
+            text=name,
+            variable=var,
+            anchor="w",
+        ).grid(
+            row=i // 2,
+            column=i % 2,
+            sticky="we",
+            padx=10,
+            pady=2
+        )
+
+    all_selected = True
+
+    toggle_all_button = tk.Button(
+        days_frame,
+        text="NONE",
+        width=10,
+        command=toggle_all_days
+    )
+    toggle_all_button.grid(
+        row=4,
+        column=0,
+        columnspan=2,
+        padx=10,
+        sticky="w"
+    )
+
+
 if __name__ == "__main__":
     # Main window
 
@@ -72,5 +142,6 @@ if __name__ == "__main__":
     # Sections
 
     create_time_section(root)
+    create_week_days_section(root)
 
     root.mainloop()
