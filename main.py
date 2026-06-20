ec1805c03dcce6b4b0fa32d6e6cc2c263f96648c
dheczko <daniel.heczko20@gmail.com>
2026-06-20 21:40:44 +0200

    Add main application window for Open-Meteo Data Visualizer

---
+++ main.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+
+
+
+if __name__ == "__main__":
+    # Main window
+
+    root = tk.Tk()
+    root.title("Open-Meteo Data Visualizer")
+    root.geometry("520x750")
+
+    root.mainloop()
